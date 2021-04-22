8d565767009e05650ca336012b1889724e168e0c
embolatado <embolatado@gmail.com>
2021-04-22 14:22:37 -0500

    views & urls Added w/ name=

--- proyectoWeb/urls.py
+++ proyectoWeb/urls.py
@@ -19,9 +19,9 @@ from webBlog.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.inicio name="Home"),
-    path('servicios', views.servicios name="Servicios"),
-    path('tienda', views.tienda name="Tienda"),
-    path('blog', views.blog name="Blog"),
-    path('contacto', views.contacto name="Contacto"),
+    path('', views.inicio, name="Home"),
+    path('servicios', views.servicios, name="Servicios"),
+    path('tienda', views.tienda, name="Tienda"),
+    path('blog', views.blog, name="Blog"),
+    path('contacto', views.contacto, name="Contacto"),
 ]
