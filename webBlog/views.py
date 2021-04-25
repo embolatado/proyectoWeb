1de757793d5f89ebac6b04069f5cf95e9230b336
embolatado <embolatado@gmail.com>
2021-04-25 18:46:13 -0500

    Reorganizado templates views urls imports include

--- webBlog/views.py
+++ webBlog/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render, HttpResponse
 
-from serviciosApp.models import Servicio
-
 # Create your views here.
 
 def inicio(request):
     return render(request, 'webBlog/index.html')
-
-
-def servicios(request):
-    cargador = Servicio.objects.all()
-
-    return render(request, 'webBlog/services.html', {"srvx": cargador})
 
 
 def tienda(request):
