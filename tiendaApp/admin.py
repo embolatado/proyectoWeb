a46e87d9987cdb2a92a816517789446bea54e63f
embolatado <embolatado@gmail.com>
2021-05-04 17:42:06 -0500

    vid.48 DB working and first products added

--- tiendaApp/admin.py
+++ tiendaApp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import categoriaProducto, Producto
 
 # Register your models here.
+
+class catProdAdm(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+
+
+class prodAdm(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+    
+
+admin.site.register(categoriaProducto, catProdAdm)
+admin.site.register(Producto, prodAdm)
+
+
